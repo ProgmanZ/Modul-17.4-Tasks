6f874e328a800d04ccaf2a09be7a25c0be9082b2
dumphill@gmail.com <Py956jch>
2021-12-14 18:32:04 +0300

    Task 01 - Готово.

--- Task-01.py
+++ Task-01.py
@@ -1,2 +1,8 @@
 # Задача 1. Анализ цен
 
+import random
+
+original_prices = [random.randint(-20, 40) for _ in range(10)]
+new_prices = [x if x > 0 else 0 for x in original_prices]
+
+print("Мы потеряли: ",  -1 * (sum(original_prices) - sum(new_prices)))
